5ced97589f173598c50037481aaa4a4ce010a55f
Renesha Matthews <reneshamatthews9@gmail.com>
2021-11-30 14:27:24 -0500

    Change var to const for all variables

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // capture references to important DOM elements
-var weatherContainer = document.getElementById('weather');
-var formEl = document.querySelector('form');
-var inputEl = document.querySelector('input');
+const weatherContainer = document.getElementById('weather');
+const formEl = document.querySelector('form');
+const inputEl = document.querySelector('input');
 
 
 formEl.onsubmit = function(e) {
@@ -9,7 +9,7 @@ formEl.onsubmit = function(e) {
   e.preventDefault();
 
   // capture user's input from form field
-  var userInput = inputEl.value.trim()
+  const userInput = inputEl.value.trim()
   // abort API call if user entered no value
   if (!userInput) return
   // call the API and then update the page
@@ -39,15 +39,15 @@ function getWeather(query) {
       // location not found, throw error/reject promise
       if (data.cod === "404") throw new Error('location not found')
       // create weather icon URL
-      var iconUrl = 'https://openweathermap.org/img/wn/' +
+      const iconUrl = 'https://openweathermap.org/img/wn/' +
         data.weather[0].icon +
         '@2x.png'
-      var description = data.weather[0].description
-      var actualTemp = data.main.temp
-      var feelsLikeTemp = data.main.feels_like
-      var place = data.name + ", " + data.sys.country
+      const description = data.weather[0].description
+      const actualTemp = data.main.temp
+      const feelsLikeTemp = data.main.feels_like
+      const place = data.name + ", " + data.sys.country
       // create JS date object from Unix timestamp
-      var updatedAt = new Date(data.dt * 1000)
+      const updatedAt = new Date(data.dt * 1000)
       // this object is used by displayWeatherInfo to update the HTML
       return {
         coords: data.coord.lat + ',' + data.coord.lon,
@@ -66,7 +66,7 @@ function displayLocNotFound() {
   // clears any previous weather info
   weatherContainer.innerHTML = "";
   // create h2, add error msg, and add to page
-  var errMsg = document.createElement('h2')
+  const errMsg = document.createElement('h2')
   errMsg.textContent = "Location not found"
   weatherContainer.appendChild(errMsg)
 }
@@ -84,24 +84,24 @@ function displayWeatherInfo(weatherObj) {
   }
 
   // weather location element
-  var placeName = document.createElement('h2')
+  const placeName = document.createElement('h2')
   placeName.textContent = weatherObj.place
   weatherContainer.appendChild(placeName)
 
   // map link element based on lat/long
-  var whereLink = document.createElement('a')
+  const whereLink = document.createElement('a')
   whereLink.textContent = "Click to view map"
   whereLink.href = "https://www.google.com/maps/search/?api=1&query=" + weatherObj.coords
   whereLink.target = "__BLANK"
   weatherContainer.appendChild(whereLink)
 
   // weather icon img
-  var icon = document.createElement('img')
+  const icon = document.createElement('img')
   icon.src = weatherObj.iconUrl
   weatherContainer.appendChild(icon)
 
   // weather description
-  var description = document.createElement('p')
+  const description = document.createElement('p')
   description.textContent = weatherObj.description
   description.style.textTransform = 'capitalize'
   weatherContainer.appendChild(description)
@@ -109,14 +109,14 @@ function displayWeatherInfo(weatherObj) {
   addBreak()
 
   // current temperature
-  var temp = document.createElement('p')
+  const temp = document.createElement('p')
   temp.textContent = "Current: " +
     weatherObj.actualTemp +
     "° F"
   weatherContainer.appendChild(temp)
 
   // "feels like" temperature
-  var feelsLikeTemp = document.createElement('p')
+  const feelsLikeTemp = document.createElement('p')
   feelsLikeTemp.textContent = "Feels like: " +
     weatherObj.feelsLikeTemp +
     "° F"
@@ -125,7 +125,7 @@ function displayWeatherInfo(weatherObj) {
   addBreak()
 
   // time weather was last updated
-  var updatedAt = document.createElement('p')
+  const updatedAt = document.createElement('p')
   updatedAt.textContent = "Last updated: " +
     weatherObj.updatedAt.toLocaleTimeString(
       'en-US',
