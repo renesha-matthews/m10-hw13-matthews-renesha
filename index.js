eb6814aab9dbaf3e0a55bbfdad781fa88cd003c1
Renesha Matthews <reneshamatthews9@gmail.com>
2021-12-01 19:38:14 -0500

    Convert then promises to async and await

--- index.js
+++ index.js
@@ -22,44 +22,43 @@ formEl.onsubmit = function(e) {
   }
 
 // calls the OpenWeather API and returns an object of weather info
-function getWeather(query) {
-    // default search to USA
-    if (!query.includes(",")) query += ',us'
-    // return the fetch call which returns a promise
-    // allows us to call .then on this function
-    return fetch(
+async function getWeather(query) {
+  // default search to USA
+  if (!query.includes(",")) query += ',us'
+  // return the fetch call which returns a promise
+  // allows us to call .then on this function
+  try {
+    const res = await fetch(
       'https://api.openweathermap.org/data/2.5/weather?q=' +
       query +
       '&units=imperial&appid=6efff70fe1477748e31c17d1c504635f'
     )
-      .then(function(res) {
-        return res.json()
-      })
-
-      .then(function(data) {
-        // location not found, throw error/reject promise
-        if (data.cod === "404") throw new Error('location not found')
-        // create weather icon URL
-        const iconUrl = 'https://openweathermap.org/img/wn/' +
-          data.weather[0].icon +
-          '@2x.png'
-        const description = data.weather[0].description
-        const actualTemp = data.main.temp
-        const feelsLikeTemp = data.main.feels_like
-        const place = data.name + ", " + data.sys.country
-        // create JS date object from Unix timestamp
-        const updatedAt = new Date(data.dt * 1000)
-        // this object is used by displayWeatherInfo to update the HTML
-        return {
-          coords: data.coord.lat + ',' + data.coord.lon,
-          description: description,
-          iconUrl: iconUrl,
-          actualTemp: actualTemp,
-          feelsLikeTemp: feelsLikeTemp,
-          place: place,
-          updatedAt: updatedAt
-        }
-      }) 
+    
+    const data = await res.json()
+    
+    // location not found, throw error/reject promise
+    if (data.cod === "404") throw new Error('location not found')
+    // create weather icon URL
+    const iconUrl = 'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png'
+    const description = data.weather[0].description
+    const actualTemp = data.main.temp
+    const feelsLikeTemp = data.main.feels_like
+    const place = data.name + ", " + data.sys.country
+    // create JS date object from Unix timestamp
+    const updatedAt = new Date(data.dt * 1000)
+     // this object is used by displayWeatherInfo to update the HTML
+      return {
+        coords: data.coord.lat + ',' + data.coord.lon,
+        description: description,
+        iconUrl: iconUrl,
+        actualTemp: actualTemp,
+        feelsLikeTemp: feelsLikeTemp,
+        place: place,
+        updatedAt: updatedAt
+      }   
+  } catch(err) {
+      console.log(err)
+  }
 }
 
 // show error message when location isn't found
